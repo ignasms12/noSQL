d04e7f1708a1bb3595cf935296a6f5861ab7e6ea
ignasms12 <56684102+ignasms12@users.noreply.github.com>
2020-10-28 18:36:43 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const redis = require('redis');
-
-const client = redis.createClient();
-
-client.get('user:9512', (err, reply)=>{
-    console.log('err is ' + err);
-    console.log('reply is ' + reply);
-});
